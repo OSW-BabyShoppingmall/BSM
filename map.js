fe9d55bf09b4c9b026081ca198fe5ece843c037d
SeongMun-Hwang <hwang105814@gmail.com>
2022-05-16 15:02:44 +0900

    add marker click event

--- map.js
+++ map.js
@@ -7,27 +7,40 @@ function myMap() {
         zoom: 14
     };
 
-
-    var markerdata=[
-    new google.maps.LatLng(36.62563026275683, 127.4544004838527), //과건물
-    new google.maps.LatLng(36.63426936439814, 127.48757506306855), //성안길점
-    new google.maps.LatLng(36.642919630018554, 127.42666365026183), //지웰시티점
+    var markerdata = [
+        new google.maps.LatLng(36.62563026275683, 127.4544004838527), //과건물
+        new google.maps.LatLng(36.63426936439814, 127.48757506306855), //성안길점
+        new google.maps.LatLng(36.642919630018554, 127.42666365026183), //지웰시티점
     ]
 
-    var infocontent=[
-        "충북대점","성안길점","지웰시티점"
+    var infocontent = [
+        "chungbuk university brach", "Seongangil brach", "gwell city brach"
     ]
 
     var map = new google.maps.Map(mapCanvas, mapOptions);
-
-    for(var i=0;i<markerdata.length;i++){
-        marker = new google.maps.Marker({
+    var marker=new Array;
+    for (var i = 0; i < markerdata.length; i++) {
+        marker[i] = new google.maps.Marker({
             position: markerdata[i],
             map: map
         });
+
         infowindow = new google.maps.InfoWindow({
             content: infocontent[i],
         });
-        infowindow.open(map,marker);
+        infowindow.open(map, marker[i]);
     }
+    marker[0].addListener('click',() => {
+        map.setZoom(18);
+        map.setCenter(markerdata[0]);
+      });
+      marker[1].addListener('click',() => {
+        map.setZoom(18);
+        map.setCenter(markerdata[1]);
+      });
+      marker[2].addListener('click',() => {
+        map.setZoom(18);
+        map.setCenter(markerdata[2]);
+      });
 }
+
