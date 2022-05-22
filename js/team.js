bd081267702f8d5641542468bd8281616930d680
the-groot <sangwoo0795@naver.com>
2022-05-22 22:42:20 +0900

    add fadein function

---
+++ js/team.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#image-frame1").mouseenter(function(){
+      $("#image-caption1").slideDown("slow");
+    });
+    $("#image-frame1").mouseleave(function(){
+      $("#image-caption1").slideUp("slow");
+    });
+  
+    $("#image-frame2").mouseenter(function(){
+      $("#image-caption2").slideDown("slow");
+    });
+    $("#image-frame2").mouseleave(function(){
+      $("#image-caption2").slideUp("slow");
+    });
+  
+    $("#image-frame3").mouseenter(function(){
+      $("#image-caption3").slideDown("slow");
+    });
+    $("#image-frame3").mouseleave(function(){
+      $("#image-caption3").slideUp("slow");
+    });
+  });
