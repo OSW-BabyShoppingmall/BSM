ec5f099cdcfe2e9c8cdb804dcdf939a0ff980fc3
the-groot <sangwoo0795@naver.com>
2022-05-23 15:09:13 +0900

    create login function by javascript

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function snsLogin(num){
+    var login;
+    if(num==0){
+        login="naver";
+    }
+    else if(num==1){
+        login="kakao";
+    }
+    else{
+        login="google";
+    }
+
+    alert("login by "+login+" is not possible yet!");
+}
+
+
+
+
+
+
+
