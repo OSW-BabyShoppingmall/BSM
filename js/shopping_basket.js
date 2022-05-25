af0126dda5cc48cf2e4af2dcbe5f5c9f4da8e76c
SeongMun-Hwang <hwang105814@gmail.com>
2022-05-25 23:09:43 +0900

    trying to add product in one function

--- js/shopping_basket.js
+++ js/shopping_basket.js
@@ -1,3 +1,5 @@
+var xxx="";
+
 $(document).ready(function () {
     $(".open_basket").click(function () {
         $("#basket").toggle("slow");
@@ -6,25 +8,50 @@ $(document).ready(function () {
 
 var product_data=[
     ["../warm.img/cloth1_1.png",".cloth1_1","0"],
-    ["cloth1_1","1"],
+    ["../warm.img/cloth1.png",".cloth1_2","0"],
     ["cloth1_2","2"],
 ]
 
-$(document).ready(function () {
+//호출한 태그의 클래스값 저장
+function get(n){
+    //var x = $(n).attr('class');
+    var x =n.className;
+    xxx="."+x;
+    alert(xxx);
+}
+
+$(document).ready(function () {   
     $(".cloth1_1").change(function () {
         if ($(".cloth1_1").is(":checked")) {
-            append(".cloth1");
+            append(".cloth1",0);
+        } else {
+            remove(".cloth1");
+        }
+    });
+});
+$(document).ready(function () {   
+    $(".cloth1_2").change(function () {
+        if ($(".cloth1_2").is(":checked")) {
+            append(".cloth1",1);
         } else {
             remove(".cloth1");
         }
     });
 });
 
-function append(name){
-    $(name).append("<tr><td><img src="+product_data[0][0]+"></td>"+
-    "<td><p>"+product_data[0][1]+"</p></td>"+
-    "<td><p>"+product_data[0][2]+"</p></td></tr>");
+function append(name,n){
+    $(name).append("<tr><td><img src="+product_data[n][0]+"></td>"+
+    "<td><p>"+product_data[n][1]+"</p></td>"+
+    "<td><p>"+product_data[n][2]+"</p></td>"+
+    "<td><p class='result'>0</p><button onclick='add()'>up</button>"+"<button>down</button></td></tr>"
+    );
 }
-function remove(name){
+
+function remove(){
     $("tr:last").remove();
+}
+
+function add(){
+    var x=document.getElementsByClassName('result');
+    x[0].innerHTML+1;
 }
