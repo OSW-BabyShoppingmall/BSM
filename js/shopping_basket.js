65b625075e1c9ee3e7cb34c3af6ac8e0988924c0
SeongMun-Hwang <hwang105814@gmail.com>
2022-05-25 17:23:03 +0900

    make function of add to shopping cart when click check box(temporary)

--- js/shopping_basket.js
+++ js/shopping_basket.js
@@ -1,5 +1,30 @@
-$(document).ready(function(){
-    $(".open_basket").click(function(){
+$(document).ready(function () {
+    $(".open_basket").click(function () {
         $("#basket").toggle("slow");
-      });   
-  });
+    });
+});
+
+var product_data=[
+    ["../warm.img/cloth1_1.png",".cloth1_1","0"],
+    ["cloth1_1","1"],
+    ["cloth1_2","2"],
+]
+
+$(document).ready(function () {
+    $(".cloth1_1").change(function () {
+        if ($(".cloth1_1").is(":checked")) {
+            append(".cloth1");
+        } else {
+            remove(".cloth1");
+        }
+    });
+});
+
+function append(name){
+    $(name).append("<tr><td><img src="+product_data[0][0]+"></td>"+
+    "<td><p>"+product_data[0][1]+"</p></td>"+
+    "<td><p>"+product_data[0][2]+"</p></td></tr>");
+}
+function remove(name){
+    $("tr:last").remove();
+}
