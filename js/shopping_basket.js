fac62283fad33f608ff60d80664d3cf2a7b9191b
SeongMun-Hwang <hwang105814@gmail.com>
2022-05-27 01:10:52 +0900

    add function of circulate price/add product in order to customer want

--- js/shopping_basket.js
+++ js/shopping_basket.js
@@ -1,10 +1,11 @@
-var xxx="";
-
+var xxx;
 $(document).ready(function () {
-    $(".open_basket").click(function () {
+    $(".btn-block").click(function () {
         $("#basket").toggle("slow");
     });
 });
+
+var product_number=0;
 
 var product_data=[
     ["../warm.img/cloth1_1.png",".cloth1_1","1"],
@@ -14,56 +15,70 @@ var product_data=[
 
 //호출한 태그의 클래스값 저장
 function get(n){
-    //var x = $(n).attr('class');
-    var x =n.className;
-    xxx="."+x;
-    alert(xxx);
+    // var x=n.className;
+    // alert(x);
+    // var y=document.getElementsByClassName(x);
+    // if(y[0].checked==true){
+    //     append(0);
+    // }
 }
 
 $(document).ready(function () {   
     $(".cloth1_1").change(function () {
-        if ($(".cloth1_1").is(":checked")) {
+        if ($(".cloth1_1").is(":checked")) {           
             append(".cloth1",0);
+            add_total(0);
         } else {
-            remove(".cloth1");
+            remove(0);
+            minus_total(0);
         }
     });
     $(".cloth1_2").change(function () {
         if ($(".cloth1_2").is(":checked")) {
             append(".cloth1",1);
+            add_total(1);
         } else {
-            remove(".cloth1");
+            remove(1);
+            minus_total(1);
         }
     });
     $(".cloth1_3").change(function () {
         if ($(".cloth1_3").is(":checked")) {
             append(".cloth1",2);
+            add_total(2);
         } else {
-            remove(".cloth1");
+            remove(2);
+            minus_total(2);
         }
     });
 });
 
 
 function append(name,n){
-    $(name).append("<tr><td><img src="+product_data[n][0]+"></td>"+
+    $(name).append("<tr class="+n+"><td><img src="+product_data[n][0]+"></td>"+
     "<td><p>"+product_data[n][1]+"</p></td>"+
+    "<td><select><option value=''>--choose option--</option>"+
+    "<option value='option1'>option1</option>"+
+    "<option value='option2'>option2</option></select></td>"+
     "<td><p class='price'>"+product_data[n][2]+"</p></td>"+
-    "<td><p class='result'>1</p><button onclick='add("+n+")'>up</button>"+
-    "<button onclick='minus("+n+")'>down</button></td></tr>"
+    "<td><span class='result'>1</span>"+
+    "<button class='btn'onclick='add("+n+")'>up</button>"+
+    "<button class='btn' onclick='minus("+n+")'>down</button></td></tr>"
     );
 }
 
-function remove(){
-    $("tr:last").remove();
+function remove(n){
+    var x=document.getElementsByClassName(n);
+    x[0].remove();
 }
 
+/*물건 개수에 따른 가격 계산*/
 function add(n){
     var x=document.getElementsByClassName("result");
     var y=document.getElementsByClassName("price");
     x[n].innerHTML = Number(x[n].innerHTML)+1;
     y[n].innerHTML=Number(y[n].innerHTML)+Number(product_data[n][2]);
-    alert(y[n].innerHTML);
+    add_total(n);
 }
 
 function minus(n){
@@ -71,5 +86,29 @@ function minus(n){
     var y=document.getElementsByClassName("price");
     x[n].innerHTML = Number(x[n].innerHTML)-1;
     y[n].innerHTML=Number(y[n].innerHTML)-Number(product_data[n][2]);
-    alert(x[n].innerHTML);
+    minus_total(n);
+}
+    
+/*물건 가격 총합, 총 결재 금액 계산*/
+function add_total(n){
+    var x=document.getElementsByClassName("product_price");   
+    var y=document.getElementsByClassName("delivery_price");
+    var z=document.getElementsByClassName("total_price");
+    x[0].innerHTML=Number(x[0].innerHTML)+Number(product_data[n][2]);
+    if(Number(x[0].innerHTML)>20){
+        y[0].innerHTML=0;
+    }
+    z[0].innerHTML=Number(x[0].innerHTML)+Number(y[0].innerHTML);
+}
+
+function minus_total(n){
+    var x=document.getElementsByClassName("product_price");   
+    var y=document.getElementsByClassName("delivery_price");
+    var z=document.getElementsByClassName("total_price");
+
+    x[0].innerHTML=Number(x[0].innerHTML)-Number(product_data[n][2]);
+    if(Number(x[0].innerHTML)<=20){
+        y[0].innerHTML=10;
+    }
+    z[0].innerHTML=Number(x[0].innerHTML)+Number(y[0].innerHTML);
 }
