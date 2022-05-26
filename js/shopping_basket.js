99fac9d058f0d54535a5981eee684b3a9a9d59c6
SeongMun-Hwang <hwang105814@gmail.com>
2022-05-26 13:49:11 +0900

    make function of increase product number by button, price change by product number

--- js/shopping_basket.js
+++ js/shopping_basket.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
 });
 
 var product_data=[
-    ["../warm.img/cloth1_1.png",".cloth1_1","0"],
-    ["../warm.img/cloth1.png",".cloth1_2","0"],
-    ["cloth1_2","2"],
+    ["../warm.img/cloth1_1.png",".cloth1_1","1"],
+    ["../warm.img/cloth1.png",".cloth1_2","2"],
+    ["../warm.img/cloth2.png",".cloth1_3","3"],
 ]
 
 //호출한 태그의 클래스값 저장
@@ -28,8 +28,6 @@ $(document).ready(function () {
             remove(".cloth1");
         }
     });
-});
-$(document).ready(function () {   
     $(".cloth1_2").change(function () {
         if ($(".cloth1_2").is(":checked")) {
             append(".cloth1",1);
@@ -37,13 +35,22 @@ $(document).ready(function () {
             remove(".cloth1");
         }
     });
+    $(".cloth1_3").change(function () {
+        if ($(".cloth1_3").is(":checked")) {
+            append(".cloth1",2);
+        } else {
+            remove(".cloth1");
+        }
+    });
 });
+
 
 function append(name,n){
     $(name).append("<tr><td><img src="+product_data[n][0]+"></td>"+
     "<td><p>"+product_data[n][1]+"</p></td>"+
-    "<td><p>"+product_data[n][2]+"</p></td>"+
-    "<td><p class='result'>0</p><button onclick='add()'>up</button>"+"<button>down</button></td></tr>"
+    "<td><p class='price'>"+product_data[n][2]+"</p></td>"+
+    "<td><p class='result'>1</p><button onclick='add("+n+")'>up</button>"+
+    "<button onclick='minus("+n+")'>down</button></td></tr>"
     );
 }
 
@@ -51,7 +58,18 @@ function remove(){
     $("tr:last").remove();
 }
 
-function add(){
-    var x=document.getElementsByClassName('result');
-    x[0].innerHTML+1;
+function add(n){
+    var x=document.getElementsByClassName("result");
+    var y=document.getElementsByClassName("price");
+    x[n].innerHTML = Number(x[n].innerHTML)+1;
+    y[n].innerHTML=Number(y[n].innerHTML)+Number(product_data[n][2]);
+    alert(y[n].innerHTML);
+}
+
+function minus(n){
+    var x=document.getElementsByClassName("result");
+    var y=document.getElementsByClassName("price");
+    x[n].innerHTML = Number(x[n].innerHTML)-1;
+    y[n].innerHTML=Number(y[n].innerHTML)-Number(product_data[n][2]);
+    alert(x[n].innerHTML);
 }
